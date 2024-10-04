8156533021e335a96d5dd79474de9396c92e59ef
Benncio09 <lorebencini13@outlook.com>
2024-10-04 07:30:34 +0200

    finito e funzionante

--- esercizio15.php
+++ esercizio15.php
@@ -6,6 +6,28 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        function stampaLista($num, $tipo){
+            $lista="";
+
+            for($i =1; $i<=$num; $i++){
+                if($i%2==0 || $i%5 ==0){
+                    $lista = $lista. "<li> $i </li>";
+                }
+            }
+
+            if($tipo == 'o'){
+                $lista="<ol>".$lista."</ol>";
+            }else{
+                $lista="<ul>".$lista."</ul>";
+            }
+
+
+            
+            echo $lista;
+        }
+        stampaLista(30, 'u');
+    ?>
     
 </body>
 </html>
